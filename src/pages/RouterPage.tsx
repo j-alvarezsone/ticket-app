78199d78c5cfad847024b34956a5756df80d31e0
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-19 09:12:08 +0200

    added router and sidebar

--- src/pages/RouterPage.tsx
+++ src/pages/RouterPage.tsx
@@ -1,7 +1,53 @@
+import { Layout, Menu } from 'antd';
+import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
+
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { ToEnter } from './ToEnter';
+import { LineUp } from './LineUp';
+import { CreateTicket } from './CreateTicket';
+import { Desk } from './Desk';
+
+const { Sider, Content } = Layout;
+
 export const RouterPage = () => {
   return (
-    <div>
-      <h1>Router Page</h1>
-    </div>
+    <Router>
+      <Layout style={{ height: '100vh' }}>
+        <Sider collapsedWidth='0' breakpoint='md'>
+          <div className='logo' />
+          <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
+            <Menu.Item key='1' icon={<UserOutlined />}>
+              <Link to='/toenter'>To enter</Link>
+            </Menu.Item>
+            <Menu.Item key='2' icon={<VideoCameraOutlined />}>
+              <Link to='/lineup'>Line up of ticket</Link>
+            </Menu.Item>
+            <Menu.Item key='3' icon={<UploadOutlined />}>
+              <Link to='/create'>Create Ticket</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className='site-layout'>
+          <Content
+            className='site-layout-background'
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Switch>
+              <Route path='/toenter' component={ToEnter} />
+              <Route path='/lineup' component={LineUp} />
+              <Route path='/create' component={CreateTicket} />
+
+              <Route path='/desk' component={Desk} />
+
+              <Redirect to='/toenter' />
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
+    </Router>
   );
 };
