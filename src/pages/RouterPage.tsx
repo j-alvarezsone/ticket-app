f292354be8d9e6d7c1f00f16c76b051a07fd57a4
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-05-21 12:38:13 +0200

    added hide to Sider from useContext

--- src/pages/RouterPage.tsx
+++ src/pages/RouterPage.tsx
@@ -1,19 +1,24 @@
+import { useContext } from 'react';
+
 import { Layout, Menu } from 'antd';
 import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
 
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+
 import { ToEnter } from './ToEnter';
 import { LineUp } from './LineUp';
 import { CreateTicket } from './CreateTicket';
 import { Desk } from './Desk';
+import { UiContext } from '../context/UiContext';
 
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
+  const { hide } = useContext(UiContext);
   return (
     <Router>
       <Layout style={{ height: '100vh' }}>
-        <Sider collapsedWidth='0' breakpoint='md'>
+        <Sider collapsedWidth='0' breakpoint='md' hidden={hide}>
           <div className='logo' />
           <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
             <Menu.Item key='1' icon={<UserOutlined />}>
